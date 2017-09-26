b8ad61d0f12b90ff339c3c573e7f8732e6c70b4d
pcmoraes <pcmoraes@gmail.com>
2017-09-26 15:30:12 -0300

    Definição de rotina para adicionar novos posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # import pdb
 
@@ -12,3 +14,20 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    # pdb.set_trace()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
